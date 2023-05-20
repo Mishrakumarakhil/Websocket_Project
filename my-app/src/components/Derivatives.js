0d22dddb15f3c6548441e8ee43c116238f5adc28
akhil <akhilmishra184@gmail.com>
2023-05-20 09:44:43 +0530

    Websocket connection for derivative data

--- my-app/src/components/Derivatives.js
+++ my-app/src/components/Derivatives.js
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import { fetchDerivativeList } from "../actions/DerivativesAction";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
+import useWebSocket from "react-use-websocket";
+import { subscribe, unSubscribe } from "../WebSocket/WebSocketUtils";
 
 const Derivatives = () => {
     const param = useParams(); //  Get the param Id param from the URL.
@@ -14,6 +16,23 @@ const Derivatives = () => {
         state.DerivativeReducer.derivativeListLoading,
     ]);
 
+    let socketURL = `wss://prototype.sbulltech.com/api/ws`;
+    const { sendJsonMessage, getWebSocket } = useWebSocket(socketURL, {
+        onOpen: () => console.log("WebSocket connection opened."),
+        onClose: () => console.log("WebSocket connection closed."),
+        shouldReconnect: (closeEvent) => true,
+        onMessage: (event) => {
+            console.log("first", event);
+            const response = JSON.parse(event.data);
+            if (response.data_type === "quote") {
+                let findObj = derivativeList.find((val) => val.token === response.payload.token);
+                if (findObj) {
+                    findObj.price = response.payload.price;
+                }
+            }
+        },
+    });
+
     useEffect(() => {
         dispatch(fetchDerivativeList(param.id));
 
@@ -22,11 +41,19 @@ const Derivatives = () => {
             dispatch(fetchDerivativeList(param.id));
         }, 30000);
 
-        //Unmount Interval
+        //Unmount Interval & close socket connection
         return () => {
             clearInterval(dataPolling);
+            getWebSocket()?.close();
         };
     }, []);
+
+    useEffect(() => {
+        if (derivativeList.length) {
+            unSubscribe(derivativeList, sendJsonMessage);
+            subscribe(derivativeList, sendJsonMessage);
+        }
+    }, [derivativeList]);
 
     return (
         <>
